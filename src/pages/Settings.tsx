e6fff6e530ce53d5aacb307a6c35550f0d623d10
sk5s <samko5sam@tutanota.com>
2023-05-11 08:44:09 +0800

    🎨 prettier

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -13,115 +13,122 @@ import {
   IonListHeader,
   IonToggle,
   IonIcon,
-  IonRouterLink
+  IonRouterLink,
 } from "@ionic/react";
 import LanguageSelectAction from "../components/LanguageSelectAction";
-import { code, contrast, help, information, logoAndroid, notifications } from 'ionicons/icons'
+import {
+  code,
+  contrast,
+  help,
+  information,
+  logoAndroid,
+  notifications,
+} from "ionicons/icons";
 import { useTranslation } from "react-i18next";
 import { Schedule } from "../lib/LocalNotification";
 import { Preferences as Storage } from "@capacitor/preferences";
-import key from '../lib/storageKey.json'
+import key from "../lib/storageKey.json";
 import { useState } from "react";
 import { trigger } from "../lib/Events";
 import { capitalize } from "../lib/Capitalize";
-import AccentColorSelectModal from '../components/AccentColorSelectModal'
-import TextColorSelectModal from '../components/TextColorSelectModal'
-import './Settings.css'
+import AccentColorSelectModal from "../components/AccentColorSelectModal";
+import TextColorSelectModal from "../components/TextColorSelectModal";
+import "./Settings.css";
 import { useHistory } from "react-router";
 import LocalizeBackButton from "../components/LocalizeBackButton";
 
-const Settings: React.FC<{accent:string}> = ({accent}) => {
-  const { t, i18n } = useTranslation()
-  const [darkChecked, setDarkChecked] = useState<boolean>()
-  const [devChecked, setDevChecked] = useState<boolean>(false)
-  const history = useHistory()
+const Settings: React.FC<{ accent: string }> = ({ accent }) => {
+  const { t, i18n } = useTranslation();
+  const [darkChecked, setDarkChecked] = useState<boolean>();
+  const [devChecked, setDevChecked] = useState<boolean>(false);
+  const history = useHistory();
 
   const getDevMode = async () => {
     const { value } = await Storage.get({ key: key.dev });
-    if (value == "true") setDevChecked(true)
-  }
-  getDevMode()
+    if (value == "true") setDevChecked(true);
+  };
+  getDevMode();
   const toggleDevModeHandler = async () => {
-    console.log("toggle dev mode")
-    let tovalue = !devChecked
+    console.log("toggle dev mode");
+    let tovalue = !devChecked;
     await Storage.set({
       key: key.dev,
       value: tovalue.toString(),
     });
-    setDevChecked(tovalue)
-    console.log(tovalue)
-    trigger("countdate_dev:change")
-  }
+    setDevChecked(tovalue);
+    console.log(tovalue);
+    trigger("countdate_dev:change");
+  };
   const testLocalNotification = async () => {
-    console.log("clicked")
+    console.log("clicked");
     await Schedule({
       title: "Countdate",
       body: "Local Notification Test",
-      id: 1
-    })
-  }
-  const darkEnable = (value:any) => {
-    let darkmodeEnable
-    let prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
-    if (value == "dark"){
-      darkmodeEnable = true
-      document.body.classList.add('dark')
+      id: 1,
+    });
+  };
+  const darkEnable = (value: any) => {
+    let darkmodeEnable;
+    let prefersDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+    if (value == "dark") {
+      darkmodeEnable = true;
+      document.body.classList.add("dark");
     } else if (value == "light") {
-      darkmodeEnable = false
+      darkmodeEnable = false;
     } else if (prefersDark) {
-      darkmodeEnable = prefersDark
-      document.body.classList.add('dark')
+      darkmodeEnable = prefersDark;
+      document.body.classList.add("dark");
     } else {
-      darkmodeEnable = prefersDark
+      darkmodeEnable = prefersDark;
     }
-    return darkmodeEnable
-  }
+    return darkmodeEnable;
+  };
   const getDarkMode = async () => {
     const { value } = await Storage.get({ key: key.theme });
-    console.log(darkEnable(value))
-    setDarkChecked(darkEnable(value))
-  }
-  getDarkMode()
+    console.log(darkEnable(value));
+    setDarkChecked(darkEnable(value));
+  };
+  getDarkMode();
   const toggleDarkModeHandler = async () => {
-    console.log(!darkChecked)
-    if (darkChecked){
+    console.log(!darkChecked);
+    if (darkChecked) {
       await Storage.set({
         key: key.theme,
-        value: "light"
-      })
-      document.body.classList.remove('dark')
+        value: "light",
+      });
+      document.body.classList.remove("dark");
     } else {
       await Storage.set({
         key: key.theme,
-        value: "dark"
-      })
-      document.body.classList.add('dark')
+        value: "dark",
+      });
+      document.body.classList.add("dark");
     }
-    setDarkChecked(!darkChecked)
-  }
+    setDarkChecked(!darkChecked);
+  };
   const darkmodeToSystem = async () => {
-    setDarkChecked(window.matchMedia('(prefers-color-scheme: dark)').matches)
-    if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
-      document.body.classList.add("dark")
+    setDarkChecked(window.matchMedia("(prefers-color-scheme: dark)").matches);
+    if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+      document.body.classList.add("dark");
     } else {
-      document.body.classList.remove("dark")
+      document.body.classList.remove("dark");
     }
     await Storage.set({
       key: key.theme,
-      value: ""
-    })
-  }
+      value: "",
+    });
+  };
 
   const handleReviewTour = async () => {
     await Storage.remove({
-      key: key.firstTime
-    })
-    history.push("/home")
+      key: key.firstTime,
+    });
+    history.push("/home");
     setTimeout(() => {
-      trigger("countdate_first:change")
+      trigger("countdate_first:change");
     }, 500);
-  }
-  
+  };
+
   return (
     <IonPage>
       <IonHeader>
@@ -144,34 +151,65 @@ const Settings: React.FC<{accent:string}> = ({accent}) => {
             <LanguageSelectAction />
           </IonItem>
           <IonItem>
-            <IonLabel onClick={toggleDevModeHandler}><IonIcon icon={code} /> {capitalize(t("toggle"))+t("between_words")+t("dev_mode")}</IonLabel>
-            <IonToggle checked={devChecked} onClick={toggleDevModeHandler} color={accent} />
-          </IonItem>
-          {(isPlatform("android") && isPlatform("hybrid")) && devChecked || (isPlatform("ios") && isPlatform("hybrid")) && devChecked ? 
-          <IonItem>
-            <IonLabel onClick={testLocalNotification}>
-              <IonIcon icon={notifications} /> {capitalize(t("test"))+t("between_words")+t("local_notification")}
+            <IonLabel onClick={toggleDevModeHandler}>
+              <IonIcon icon={code} />{" "}
+              {capitalize(t("toggle")) + t("between_words") + t("dev_mode")}
             </IonLabel>
+            <IonToggle
+              checked={devChecked}
+              onClick={toggleDevModeHandler}
+              color={accent}
+            />
           </IonItem>
-           : ''
-          }
+          {(isPlatform("android") && isPlatform("hybrid") && devChecked) ||
+          (isPlatform("ios") && isPlatform("hybrid") && devChecked) ? (
+            <IonItem>
+              <IonLabel onClick={testLocalNotification}>
+                <IonIcon icon={notifications} />{" "}
+                {capitalize(t("test")) +
+                  t("between_words") +
+                  t("local_notification")}
+              </IonLabel>
+            </IonItem>
+          ) : (
+            ""
+          )}
           <IonItem>
-            <IonLabel onClick={handleReviewTour}><IonIcon icon={help} /> {t("review_tour")}</IonLabel>
+            <IonLabel onClick={handleReviewTour}>
+              <IonIcon icon={help} /> {t("review_tour")}
+            </IonLabel>
           </IonItem>
           <IonRouterLink routerLink="/about">
             <IonItem>
-              <IonLabel color="dark"><IonIcon icon={information} /> {capitalize(t("about"))} Countdate</IonLabel>
+              <IonLabel color="dark">
+                <IonIcon icon={information} /> {capitalize(t("about"))}{" "}
+                Countdate
+              </IonLabel>
             </IonItem>
           </IonRouterLink>
           <IonListHeader lines="none" color={accent}>
             <IonLabel>{capitalize(t("theme"))}</IonLabel>
           </IonListHeader>
           <IonItem>
-            <IonLabel onClick={toggleDarkModeHandler}><IonIcon icon={contrast} /> {capitalize(t("toggle"))+t("between_words")+t("dark_mode")}</IonLabel>
-            <IonToggle checked={darkChecked} onClick={toggleDarkModeHandler} color={accent} />
+            <IonLabel onClick={toggleDarkModeHandler}>
+              <IonIcon icon={contrast} />{" "}
+              {capitalize(t("toggle")) + t("between_words") + t("dark_mode")}
+            </IonLabel>
+            <IonToggle
+              checked={darkChecked}
+              onClick={toggleDarkModeHandler}
+              color={accent}
+            />
           </IonItem>
           <IonItem>
-            <IonLabel onClick={darkmodeToSystem}><IonIcon icon={logoAndroid} /> {capitalize(t("follow"))+t("between_words")+t("system")+t("between_words")+t("dark_mode")}</IonLabel>
+            <IonLabel onClick={darkmodeToSystem}>
+              <IonIcon icon={logoAndroid} />{" "}
+              {capitalize(t("follow")) +
+                t("between_words") +
+                t("system") +
+                t("between_words") +
+                t("dark_mode")}
+            </IonLabel>
           </IonItem>
           <IonItem>
             <AccentColorSelectModal />
