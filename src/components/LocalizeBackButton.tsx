78f14a8a8c3aef73f8c3f9aaf5cd2a0126337591
sk5s <samko5sam@tutanota.com>
2023-05-13 06:55:00 +0800

    💄

--- src/components/LocalizeBackButton.tsx
+++ src/components/LocalizeBackButton.tsx
@@ -1,12 +1,13 @@
 import { IonButtons, IonBackButton } from "@ionic/react";
 import { useTranslation } from "react-i18next";
+import { capitalize } from "../lib/Capitalize";
 
 export default function LocalizeBackButton({ color }: { color: string }) {
   const { t } = useTranslation();
   return (
     <>
       <IonButtons slot="start">
-        <IonBackButton text={t("back")} color={color}></IonBackButton>
+        <IonBackButton text={capitalize(t("back"))} color={color}></IonBackButton>
       </IonButtons>
     </>
   );
