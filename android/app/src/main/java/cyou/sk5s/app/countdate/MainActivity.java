e8da844ae5b49af7900e82193c1e0a2678460c4c
sk5s <samko5sam@tutanota.com>
2024-02-16 23:49:43 +0800

    💄 Remove scroll bounce

--- android/app/src/main/java/cyou/sk5s/app/countdate/MainActivity.java
+++ android/app/src/main/java/cyou/sk5s/app/countdate/MainActivity.java
@@ -2,4 +2,15 @@ package cyou.sk5s.app.countdate;
 
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+  }
+  @Override
+  public void onStart() {
+    super.onStart();
+    WebView webview = getBridge().getWebView();
+    webview.setOverScrollMode(WebView.OVER_SCROLL_NEVER);
+  }
+}
