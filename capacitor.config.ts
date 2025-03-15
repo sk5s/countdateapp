341c3086ae570d8e56f8d5e7441686b62b256d1e
sk5s <samko5sam@tutanota.com>
2025-03-15 20:19:25 +0800

    👌 Migrate CRA to Vite, Remove service worker for the web.

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,8 +3,10 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'cyou.sk5s.app.countdate',
   appName: 'Countdate',
-  webDir: 'build',
-  bundledWebRuntime: false,
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  },
   plugins: {
     LocalNotifications: {
       smallIcon: "countdateapp-logo",
